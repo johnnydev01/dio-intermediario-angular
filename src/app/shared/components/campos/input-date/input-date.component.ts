9e7e462a875ed444be8c455e63842925fb70c52a
Johnny Silva <johnnysilva.dev@gmail.com>
2022-02-22 15:15:04 -0300

    componentizando inputs parte2

--- src/app/shared/components/campos/input-date/input-date.component.ts
+++ src/app/shared/components/campos/input-date/input-date.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, AbstractControl } from '@angular/forms';
+import { ValidarCamposService } from '../validar-campos.service';
 
 @Component({
   selector: 'dio-input-date',
   templateUrl: './input-date.component.html',
   styleUrls: ['./input-date.component.scss']
 })
-export class InputDateComponent implements OnInit {
+export class InputDateComponent  {
 
-  constructor() { }
+  @Input() titulo: string;
+  @Input() formGroup: FormGroup;
+  @Input() controlName: string;
 
-  ngOnInit() {
+  constructor(public validacao: ValidarCamposService) { }
+
+
+  get formControl(): AbstractControl {
+    return this.formGroup.controls[this.controlName];
   }
+
 
 }
